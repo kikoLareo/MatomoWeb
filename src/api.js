d1f5126bd468902c50b2680a1124e99c43dc3999
Francisco Lareo <francisco.lareo@gmail.com>
2024-07-11 19:04:38 +0200

    New api

--- src/api.js
+++ src/api.js
@@ -8,8 +8,7 @@ const api = axios.create({
 // Función para obtener datos del IPC del INE
 export const getIpcData = async () => {
   try {
-    const response = await api.get('index.php?date=2024-07-10&viewDataTable=graphEvolution&dateUsedInGraph=2024-06-11%2C2024-07-10&evolution_day_last_n=30&columns=nb_plays&module=MediaAnalytics&action=getEvolutionGraph&disableLink=1&widget=1&moduleToWidgetize=MediaAnalytics&actionToWidgetize=getEvolutionGraph&idSite=9&period=week&totals=0'); // Ajusta según sea necesario
-    console.log('IPC Data:', response.data);
+    const response = await api.get('index.php?date=2024-07-02&module=Annotations&action=getEvolutionIcons&idSite=1&period=day&filter_limit=-1&lastN=30'); // Ajusta según sea necesario
     return response.data;
   } catch (error) {
     console.error('Error fetching IPC data:', error);
