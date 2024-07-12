4ef526991e7f13a63329afd34289c538f318d005
Francisco Lareo <francisco.lareo@gmail.com>
2024-07-12 18:13:42 +0200

    Llamadas a la api de matomo

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
 // Función para obtener datos del IPC del INE
 export const getIpcData = async () => {
   try {
-    const response = await api.get('index.php?date=2024-07-02&module=Annotations&action=getEvolutionIcons&idSite=1&period=day&filter_limit=-1&lastN=30'); // Ajusta según sea necesario
+    const response = await api.get(graficaUrl); // Ajusta según sea necesario
     return response.data;
   } catch (error) {
     console.error('Error fetching IPC data:', error);
@@ -17,3 +17,134 @@ export const getIpcData = async () => {
 };
 
 export default api;
+
+/**
+ * // $(document).ready( async function() {
+
+    // let matomoURL1 = `https://tiivii-ott.matomo.cloud/index.php?module=API&format=JSON&idSite=${idSite}&period=range&date=${inputFechaInicio.value},${inputFechaFin.value}&method=MediaAnalytics.getVideoTitles&filter_column=label&filter_pattern=ID%3A+${petiscoID}&secondaryDimension=spent_time&format_metrics=1&expanded=1&token_auth=${token}`;
+    // const response_matomoURL1 = await fetch(matomoURL1)
+    // const Data_0_matomoURL1 = response_matomoURL1[0]
+    // const Data_matomoURL1 = await Data_0_matomoURL1.json();
+
+    // console.log(Data_matomoURL1)
+
+    // let matomoURL2 = `https://tiivii-ott.matomo.cloud/index.php?module=API&format=JSON&idSite=${idSite}&period=range&date=${inputFechaInicio.value},${inputFechaFin.value}&method=MediaAnalytics.getVideoTitles&filter_column=label&filter_pattern=ID%3A+${petiscoID}&secondaryDimension=media_progress&format_metrics=1&expanded=1&token_auth=${token}`
+    // const response_matomoURL2 = await fetch(matomoURL2)
+    // const Data_0_matomoURL2 = response_matomoURL2[0]
+    // const Data_matomoURL2 = await Data_0_matomoURL2.json();
+
+    // console.log(Data_matomoURL2)
+
+    // const labels = Data_matomoURL1.map(item => item.label);
+    // const nbPlays = Data_matomoURL1.map(item => item.nb_plays || 0);
+
+    // const labels2 = Data_matomoURL2.map(item => item.label);
+    // const nbPlays2 = Data_matomoURL2.map(item => item.nb_plays || 0);
+
+    // //**charts */
+    // $(function () {
+    // //** Data and config for chartjs */
+    // 'use strict';
+    // if (labels.length === 0 || nbPlays.length === 0) {
+    //     // Mostrar mensaje de "No hay datos"
+    //     $("#barChart").html("<p>No hay datos disponibles.</p>");
+    // } else {
+    //     var barChartData = {
+    //         labels: labels,
+    //         datasets: [{
+    //             label: 'total',
+    //             data: nbPlays,
+    //             backgroundColor: 'rgba(255, 66, 15, 0.7)',
+    //             borderColor: 'rgba(255, 66, 15, 0.7)'
+    //         }]
+    //     }; //data
+    //     var barChartOptions = {
+    //         scales: {
+    //             xAxes: [{
+    //                 ticks: {
+    //                     beginAtZero: true,
+    //                     fontSize: 20,
+    //                 }
+    //             }],
+    //             yAxes: [{
+    //                 ticks: {
+    //                     beginAtZero: true,
+    //                     fontSize: 20,
+    //                     precision: 0, // Mostrar solo números enteros en el eje Y
+    //                 }
+    //             }]
+    //         },
+    //         legend: {
+    //             display: false
+    //         },
+    //         tooltips: {
+    //             enabled: true,
+    //             titleFontSize: 26, // Tamaño de la fuente del título del tooltip
+    //             bodyFontSize: 24 // Tamaño de la fuente del cuerpo del tooltip
+    //         }
+    //     }; //options
+
+    //     // Get context with jQuery - using jQuery's .get() method.
+    //     var barChartCanvas = $("#barChart").get(0).getContext("2d");
+    //     // This will get the first returned node in the jQuery collection.
+    //     var barChart = new Chart(barChartCanvas, {
+    //         type: 'bar',
+    //         data: barChartData,
+    //         options: barChartOptions
+    //     });    
+    // }
+    // if (labels2.length === 0 || nbPlays2.length === 0) {
+    //     // Mostrar mensaje de "No hay datos"
+    //     $("#barChart2").html("<p>No hay datos disponibles.</p>");
+    // } else {
+    //     var barChart2Data = {
+    //         labels: labels2,
+    //         datasets: [{
+    //             label: 'total',
+    //             data: nbPlays2,
+    //             backgroundColor: 'rgba(255, 66, 15, 0.7)',
+    //             borderColor: 'rgba(255, 66, 15, 0.7)'
+    //         }]
+    //     }; //data
+    //     var barChart2Options = {
+    //         scales: {
+    //             xAxes: [{
+    //                 ticks: {
+    //                     beginAtZero: true,
+    //                     fontSize: 20,
+    //                 }
+    //             }],
+    //             yAxes: [{
+    //                 ticks: {
+    //                     beginAtZero: true,
+    //                     fontSize: 20,
+    //                     precision: 0, // Mostrar solo números enteros en el eje Y
+    //                 }
+    //             }]
+    //         },
+    //         legend: {
+    //             display: false
+    //         },
+    //         tooltips: {
+    //             enabled: true,
+    //             titleFontSize: 26, // Tamaño de la fuente del título del tooltip
+    //             bodyFontSize: 24 // Tamaño de la fuente del cuerpo del tooltip
+    //         }
+    //     }; //options
+
+    //     // Get context with jQuery - using jQuery's .get() method.
+    //     var barChart2Canvas = $("#barChart2").get(0).getContext("2d");
+    //     // This will get the first returned node in the jQuery collection.
+    //     var barChart2 = new Chart(barChart2Canvas, {
+    //         type: 'bar',
+    //         data: barChart2Data,
+    //         options: barChart2Options
+    //     });
+    // }
+    
+    // }); //chart
+    // });
+
+    
+
+ 
---
+++ src/config/Urls.js
@@ -0,0 +1,1 @@
+const graficaUrl= 'index.php?module=API&format=JSON&idSite=2&period=range&date=2023-12-01,2024-07-01&method=MediaAnalytics.getVideoTitles&filter_column=label&filter_pattern=ID%3A+377&secondaryDimension=spent_time&format_metrics=1&expanded=1&token_auth=87148f8d4f8a9f7e5e21f6bd705bfdbd';
