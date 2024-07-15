95679e38627653f9d03ba5dfee15b313e906b20c
kikoLareo <kiko.lareo@gmail.com>
2024-07-15 16:44:48 +0200

    Refactor API call to use async/await syntax

--- src/config/Urls.js
+++ src/config/Urls.js
@@ -1,1 +1,5 @@
-const graficaUrl= 'index.php?module=API&format=JSON&idSite=2&period=range&date=2023-12-01,2024-07-01&method=MediaAnalytics.getVideoTitles&filter_column=label&filter_pattern=ID%3A+377&secondaryDimension=spent_time&format_metrics=1&expanded=1&token_auth=87148f8d4f8a9f7e5e21f6bd705bfdbd';
+
+const token_auth ='87148f8d4f8a9f7e5e21f6bd705bfdbd';
+const graficaUrl= 'index.php?module=API&format=JSON&idSite=2&period=range&date=2023-12-01,2024-07-01&method=MediaAnalytics.getVideoTitles&filter_column=label&filter_pattern=ID%3A+377&secondaryDimension=spent_time&format_metrics=1&expanded=1&token_auth=' + token_auth;
+
+export default graficaUrl;
