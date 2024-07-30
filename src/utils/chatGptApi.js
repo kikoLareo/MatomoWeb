4a09cedbef3cfe063b90a0def4f6063523ec7c7a
Francisco Lareo <francisco.lareo@gmail.com>
2024-07-30 19:45:27 +0200

    Ajuste en el promt para el Api

--- src/utils/chatGptApi.js
+++ src/utils/chatGptApi.js
@@ -4,9 +4,9 @@ const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
 
 export const fetchChartAnalysis = async ({ title, description, data }) => {
     const conversationContext = [
-    { role: 'system', content: 'Las respuestas deben ser en español y deben proporcionar un análisis detallado de los datos.' },
-    { role: 'user', content: `Analiza la siguiente gráfica titulada "${title}". ${description} Aquí están los datos: ${JSON.stringify(data)}` }
-  ];
+        { role: 'system', content: 'Las respuestas deben ser en español y deben proporcionar un análisis detallado y comprensible de los datos proporcionados. Asegúrate de considerar el contexto de la gráfica y la descripción proporcionada.' },
+        { role: 'user', content: `Analiza la siguiente gráfica titulada "${title}". ${description} Los datos son los siguientes: ${JSON.stringify(data)}. Por favor, proporciona un análisis detallado considerando tendencias, patrones y posibles implicaciones de estos datos.` }
+      ];
 
   try {
     const response = await axios.post('https://api.openai.com/v1/chat/completions', {
