87ad10c9a12be97f56e72779acdff95e3c5131d1
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-08 23:16:01 +0200

    refactor: Update PieChartComponent to filter out items with zero visits and calculate percentages

--- src/components/PieChartComponent.js
+++ src/components/PieChartComponent.js
@@ -3,15 +3,52 @@ import React from 'react';
 import { Pie } from 'react-chartjs-2';
 
 const PieChartComponent = ({ data }) => {
+  // Filter out items with nb_visits equal to 0
+  const filteredData = data.filter(item => item.nb_visits > 0);
+
+  // Extract labels and values
+  const labels = filteredData.map(item => item.label);
+  const values = filteredData.map(item => item.nb_visits);
+
+  // Calculate total visits
+  const totalVisits = values.reduce((acc, value) => acc + value, 0);
+
+  // Calculate percentages
+  const percentages = values.map(value => ((value / totalVisits) * 100).toFixed(2));
+
+  const chartData = {
+    labels: labels,
+    datasets: [
+      {
+        data: percentages,
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)',
+          'rgba(153, 102, 255, 0.6)',
+          'rgba(255, 159, 64, 0.6)',
+        ],
+        borderColor: [
+          'rgba(75, 192, 192, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   const options = {
     plugins: {
       tooltip: {
         callbacks: {
           label: function (tooltipItem) {
-            const total = tooltipItem.dataset.data.reduce((acc, value) => acc + value, 0);
             const currentValue = tooltipItem.raw;
-            const percentage = ((currentValue / total) * 100).toFixed(2);
-            return `${tooltipItem.label}: ${percentage}%`;
+            return `${tooltipItem.label}: ${currentValue}%`;
           },
         },
       },
@@ -21,7 +58,7 @@ const PieChartComponent = ({ data }) => {
   return (
     <div className="pie-chart">
       <h2>Pie Chart</h2>
-      <Pie data={data} options={options} />
+      <Pie data={chartData} options={options} />
     </div>
   );
 };
