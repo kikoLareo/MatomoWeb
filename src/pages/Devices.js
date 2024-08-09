194f51e9b3287d28b594dbbd0809afb82ddd5dde
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-09 17:28:40 +0200

    refactor: Remove JavaScript fallback message in index.html and update Devices.js

--- src/pages/Devices.js
+++ src/pages/Devices.js
@@ -3,20 +3,31 @@ import React, { useContext, useState, useEffect } from 'react';
 import { IdSiteContext } from '../contexts/idSiteContext';
 import { fetchDataForCharts } from '../utils/fetchDataHelper';
 import PieChartComponent from '../components/PieChartComponent';
-import {devicesDetectionCharts} from '../config/chartsConfig';
+import { devicesDetectionCharts } from '../config/chartsConfig';
 
 const Devices = () => {
   const { idSite } = useContext(IdSiteContext);
   const [chartData, setChartData] = useState({});
 
   useEffect(() => {
+    console.log('useEffect is running');
+    console.log('idSite:', idSite);
+
     const fetchData = async () => {
-      const data = await fetchDataForCharts(idSite, devicesDetectionCharts);
-      console.log(data);
-      setChartData(data);
+      try {
+        console.log('Fetching data for charts');
+        console.log('devicesDetectionCharts:', devicesDetectionCharts);
+        const data = await fetchDataForCharts(idSite, devicesDetectionCharts);
+        console.log('Fetched data:', data);
+        setChartData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
-    fetchData();
+    if (idSite) {
+      fetchData();
+    }
   }, [idSite]);
 
   return (
