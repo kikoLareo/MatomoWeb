84e85ad72fa6418fcc9d8b56cfa2ee4c1c506604
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-12 18:43:33 +0200

    Arreglo en la comprobacion de porcentajes menores que 1

--- src/components/PieChartComponent.js
+++ src/components/PieChartComponent.js
@@ -3,32 +3,32 @@ import React from 'react';
 import { Pie } from 'react-chartjs-2';
 
 const PieChartComponent = ({ labels, data, title, description }) => {
-  var others = 0;
-  // Calculate total visits
+  let others = 0;
   const totalVisits = data.reduce((acc, value) => acc + value, 0);
 
-  // Calculate percentages
-  const percentages = data.map(value => {
-    var per = ((value / totalVisits) * 100).toFixed(2);
-    if(per < 1){
-      others += per;
-      data = data.filter(item => item !== value);
-      return 0;
-    }else{
-      return per;
+  const filteredData = [];
+  const filteredLabels = [];
+
+  data.forEach((value, index) => {
+    const per = ((value / totalVisits) * 100).toFixed(2);
+    if (per < 1) {
+      others += parseFloat(per);
+    } else {
+      filteredData.push(parseFloat(per));
+      filteredLabels.push(labels[index]);
     }
   });
-  
-  if(others > 0){
-    labels.push("Otros");
+
+  if (others > 0) {
+    filteredLabels.push("Otros");
+    filteredData.push(others);
   }
 
-
   const chartData = {
-    labels: labels,
+    labels: filteredLabels,
     datasets: [
       {
-        data: percentages.filter(item => item !== 0).concat(others),
+        data: filteredData,
         backgroundColor: [
           'rgba(75, 192, 192, 0.6)',
           'rgba(54, 162, 235, 0.6)',
@@ -66,7 +66,7 @@ const PieChartComponent = ({ labels, data, title, description }) => {
         },
       },
     },
-    animation:{
+    animation: {
       animateScale: true,
     },
   };
@@ -76,10 +76,9 @@ const PieChartComponent = ({ labels, data, title, description }) => {
       <div className='chart-description'>
         <h2>{title}</h2>
       </div>
-      <div style={{width: '100%', height: '40vh', margin: 'auto'}}>
+      <div style={{ width: '100%', height: '40vh', margin: 'auto' }}>
         <Pie data={chartData} options={options} />
       </div>
-    
     </div>
   );
 };
