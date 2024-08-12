6f5f6a59288de129905603291de56b68516deb94
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-13 00:05:16 +0200

    refactor: Update getGraph.js to dynamically render multiple charts based on chart type and data

--- src/utils/getGraph.js
+++ src/utils/getGraph.js
@@ -3,12 +3,12 @@ import ChartComponent from "../components/ChartComponent";
 import ChartInfo from "../components/ChartInfo";
 import PieChartComponent from "../components/PieChartComponent";
 
-function useGraph(chart, idSite) { // Acepta idSite como argumento
+function useGraph(charts, idSite) { // Acepta idSite como argumento
 
     useEffect(() => {
         const fetchData = async () => {
             await Promise.all(
-                chart.map(async (chartItem) => {
+                charts.map(async (chartItem) => {
                     console.log('Fetching data for chart:', chartItem, idSite);
                     await chartItem.getData(idSite);
                     console.log('Fetched data for chart:', chartItem);
@@ -16,63 +16,65 @@ function useGraph(chart, idSite) { // Acepta idSite como argumento
             );
         };
         fetchData();
-    }, [idSite, chart]);
+    }, [idSite, charts]);
 
-    if (!chart) {
+    if (!charts || charts.length === 0) {
         return <p>Loading...</p>;
     }
 
-    var { type, data, title, description, module, action, metrics } = chart;
+    return charts.map((chart, chartIndex) => {
+        var { type, data, title, description, module, action, metrics } = chart;
 
-    console.log('getGraph:', chart);
-    if (metrics) {
-        const preparedData = Object.keys(data.value).map(item => {
-            let result = {};
-            metrics.forEach(metric => {
-                result[metric] = data.value[item][metric] || 0;
+        console.log('getGraph:', chart);
+        if (data.info.result === 'error') {
+            return <p key={chartIndex}>Error: {data.info.message}</p>;
+        }
+
+        if (metrics) {
+            const preparedData = Object.keys(data.value).map(item => {
+                let result = {};
+                metrics.forEach(metric => {
+                    result[metric] = data.value[item][metric] || 0;
+                });
+                return result;
             });
-            return result;
-        });
 
-        console.log('getGraph:', preparedData);
+            console.log('getGraph:', preparedData);
 
-        switch (type) {
-            case 'lineal':
-                return (
-                    preparedData.map((item, index) => (
-                        <div className="graph_component" key={index}>
+            switch (type) {
+                case 'lineal':
+                    return (
+                        <div className="graph_component" key={chartIndex}>
                             <ChartComponent
-                                data={{ labels: Object.keys(item), data: Object.values(item), title: title }}
+                                data={{ labels: Object.keys(preparedData[0]), data: preparedData.map(d => Object.values(d)), title: title }}
                                 title={title}
                             />
                             <ChartInfo
                                 title={title}
                                 description={description}
-                                data={item}
+                                data={preparedData}
                                 module={module}
                                 action={action}
                             />
                         </div>
-                    ))
-                );
-                
-            case 'pie':
-                return (
-                    preparedData.map((item, index) => (
-                        <div className="graph_component" key={index}>
+                    );
+
+                case 'pie':
+                    return (
+                        <div className="graph_component" key={chartIndex}>
                             <PieChartComponent
-                                labels={Object.keys(item)}
-                                data={Object.values(item)}
+                                labels={Object.keys(preparedData[0])}
+                                data={preparedData.map(d => Object.values(d))}
                                 title={title}
                             />
                         </div>
-                    ))
-                );
-               
-            default:
-                return <p>Unsupported chart type</p>;
+                    );
+
+                default:
+                    return <p key={chartIndex}>Unsupported chart type</p>;
+            }
         }
-    }
+    });
 }
 
 export default useGraph;
