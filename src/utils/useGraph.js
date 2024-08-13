49228fe3e6214b8be9d9fa59020ac6c197f391b2
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-13 19:13:51 +0200

    refactor: Update useGraph.js to transform data before rendering charts

--- src/utils/useGraph.js
+++ src/utils/useGraph.js
@@ -4,6 +4,24 @@ import { fetchChartData } from './fetchChartData';
 import GraphRenderer from './GraphRenderer';
 import { titles } from './dictionaryMetrics/metricsTitles';
 
+function transformData(data, metrics) {
+  const result = [];
+
+  for (const [date, values] of Object.entries(data)) {
+      if (Array.isArray(values) && values.length === 0) continue; // Skip empty arrays
+
+      const entry = { fecha: date };
+      metrics.forEach(metric => {
+          entry[metric] = values[metric] || 0; // Default to 0 if metric is not present
+      });
+
+      result.push(entry);
+  }
+
+  return result;
+}
+
+
 function useGraph(charts, idSite) {
   const [chartData, setChartData] = useState([]);
 
@@ -20,6 +38,8 @@ function useGraph(charts, idSite) {
   }
   return charts.map((chart, chartIndex) => {
     const { metrics, data, description, title, type } = chart;
+    const formatedData = transformData(data, metrics);
+
     return (
       <div key={chartIndex}>
         <h3>{title}</h3>
@@ -31,9 +51,7 @@ function useGraph(charts, idSite) {
               key={`${chartIndex}-${metricIndex}`}
               chart={{
                 type,
-                data: Object.keys(data).map(key => ({
-                  [metric]: data[key][metric] || 0
-                })),
+                data: formatedData,
                 title: data.info.columns? data.info.columns[metric] : data.info.metadata? data.info.metadata.metrics[metric]: titles[metric]
                 
               }}
