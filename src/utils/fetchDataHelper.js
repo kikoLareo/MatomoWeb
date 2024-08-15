74cd3197e198601739942d5469041f08300dc164
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-15 19:44:16 +0200

    refactor: Update fetchDataHelper.js to convert responseData to an array

--- src/utils/fetchDataHelper.js
+++ src/utils/fetchDataHelper.js
@@ -65,7 +65,7 @@ export const fetchData = async (idSite, requestData) => {
 
     
     const data = {
-      value: responseData ,
+      value: responseData.toArray(),
       info: processedData ? processedData : {}
     };
 
