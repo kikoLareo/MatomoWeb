f2cab150e1de377e90a96f511db948c37bec3d79
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-17 11:44:54 +0200

    refactor: Update DataOverviewTable component to handle missing params

--- src/components/tableComponent.js
+++ src/components/tableComponent.js
@@ -4,7 +4,7 @@ import { IdSiteContext } from '../contexts/idSiteContext';
 import FilterMinutes from './LastMinutesFilter';
 import { titles } from '../utils/dictionaryMetrics/metricsTitles';
 
-export const DataOverviewTable = ({ fetchDataFunction, params = ["period", "date"] }) => {
+export const DataOverviewTable = ({ fetchDataFunction, params}) => {
   const [data, setData] = useState(null);
   const [metadata, setMetadata] = useState({});
   const [period, setPeriod] = useState('day');
@@ -15,6 +15,9 @@ export const DataOverviewTable = ({ fetchDataFunction, params = ["period", "date
   
   const memoizedParams = useMemo(() => params, [params]);
 
+  if(!params) {
+    params = ["period", "date"];
+  }
   useEffect(() => {
     const fetchData = async () => {
       try {
