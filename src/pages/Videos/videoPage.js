94efd9599d8bc18fbea67e53fccbe9292bcc7cd7
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-18 13:19:22 +0200

    refactor: Update videoPageTableConfig to fetch data for the specified site ID

--- src/pages/Videos/videoPage.js
+++ src/pages/Videos/videoPage.js
@@ -1,48 +1,46 @@
 import React, { useEffect, useContext, useState } from 'react';
 import DataTable from '../../components/DataTableComponent';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 import { IdSiteContext } from '../../contexts/idSiteContext';
 import { videoPageTableConfig } from './videosPageConfig';
 import { setTitle } from '../../components/Header';
 
 const VideoDataPage = () => {
-    const { idSite } = useContext(IdSiteContext);
-    const [chartConfig, setChartConfig] = useState(videoPageTableConfig);
-    console.log(idSite);
-  console.log(videoPageTableConfig);
-  console.log("Video page data: ", chartConfig);
+  const { idSite } = useContext(IdSiteContext);
+  const [chartConfig, setChartConfig] = useState(videoPageTableConfig);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setTitle(videoPageTableConfig.title);
-    }, []);
-
-    // useEffect(() => {
-    //     await videoPageTableConfig.getData(idSite);
-    //     console.log("Updated config: ", videoPageTableConfig);
-    //     setChartConfig({ ...videoPageTableConfig });
-    // },[]);
+  }, []);
 
   useEffect(() => {
-      console.log("Loading data for site: ", idSite);
-      const loadData = async () => {
-          try {
-            console.log("Loading data for site: ", idSite);
-            const updatedConfig = await videoPageTableConfig.getData(idSite);
-            console.log("Updated config: ", updatedConfig);
-            setChartConfig({ ...updatedConfig });
-          } catch (error) {
-            console.error("Error loading data: ", error);
-          }
-        };
-      
-        loadData();
-        
-      }, [idSite]);
-    
+    console.log("Loading data for site: ", idSite);
+    const loadData = async () => {
+      try {
+        const updatedConfig = await videoPageTableConfig.getData(idSite);
+        setChartConfig({ ...updatedConfig });
+      } catch (error) {
+        console.error("Error loading data: ", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadData();
+  }, [idSite]);
+
   return (
     <div>
       <h1>{chartConfig.title}</h1>
-      <DataTable chart={chartConfig} />
+      {loading ? (
+        <Skeleton count={5} height={40} />
+      ) : (
+        <DataTable chart={chartConfig} />
+      )}
     </div>
   );
 };
+
 export default VideoDataPage;
