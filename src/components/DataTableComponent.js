d52e54000f042298dc2b771b07e722fd03d7f3aa
Francisco Lareo <francisco.lareo@gmail.com>
2024-08-18 13:37:35 +0200

    refactor: Update DataTableComponent to enable column sorting and improve table styling

--- src/components/DataTableComponent.js
+++ src/components/DataTableComponent.js
@@ -1,59 +1,61 @@
 import React, { useState, useEffect } from 'react';
+import './Table.css'; // Asegúrate de importar el archivo CSS
 
 const DataTable = ({ chart }) => {
     var data = chart.data.value;
-  const [sortedData, setSortedData] = useState(data);
-  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
+    const [sortedData, setSortedData] = useState(data);
+    const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
 
-  console.log("Video page data: ", data, chart);
-  useEffect(() => {
-    setSortedData(data);
-  }, [data]);
+    console.log("Video page data: ", data, chart);
+    useEffect(() => {
+        setSortedData(data);
+    }, [data]);
 
-  const sortData = (key) => {
-    let direction = 'ascending';
-    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
-      direction = 'descending';
-    }
+    const sortData = (key) => {
+        let direction = 'ascending';
+        if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+            direction = 'descending';
+        }
 
-    const sorted = [...sortedData].sort((a, b) => {
-      if (a[key] < b[key]) {
-        return direction === 'ascending' ? -1 : 1;
-      }
-      if (a[key] > b[key]) {
-        return direction === 'ascending' ? 1 : -1;
-      }
-      return 0;
-    });
+        const sorted = [...sortedData].sort((a, b) => {
+            if (a[key] < b[key]) {
+                return direction === 'ascending' ? -1 : 1;
+            }
+            if (a[key] > b[key]) {
+                return direction === 'ascending' ? 1 : -1;
+            }
+            return 0;
+        });
 
-    setSortedData(sorted);
-    setSortConfig({ key, direction });
-  };
+        setSortedData(sorted);
+        setSortConfig({ key, direction });
+    };
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          {chart.columns.map((column) => (
-            <th key={column.key} 
-                onClick={() => sortData(column.key)}
-                >
-              {column.label}
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {sortedData.map((item, index) => (
-          <tr key={index}>
-            {chart.columns.map((column) => (
-              <td key={column.key}>{item[column.key]}</td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {chart.columns.map((column) => (
+                        <th key={column.key} 
+                            onClick={() => sortData(column.key)}
+                            className="sortable-column"
+                        >
+                            {column.label}
+                        </th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {sortedData.map((item, index) => (
+                    <tr key={index}>
+                        {chart.columns.map((column) => (
+                            <td key={column.key}>{item[column.key]}</td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 };
 
 export default DataTable;
